2c5c12ba983a42df8569d5f887135d9a59bd6393
rhornbuckle3 <russspringfield@gmail.com>
2018-02-27 09:32:31 -0500

    Split some repetitive code out into their own methods. Working on new classifier.

--- fullStackClustering.py
+++ fullStackClustering.py
@@ -83,7 +83,7 @@ def gradientAscent(eta,tol,iBeta,xSet,ySet):
             return iBeta
         itera=itera+1
 #construct the model for deploying log regression, details on whiteboard
-def fullModel():
+def safeLabels():
     clusterCenter=clusterHOME(2)
     clusterOne=clusterCenter[0]
     clusterTwo=clusterCenter[1]
@@ -93,27 +93,30 @@ def fullModel():
     for i in range(0,np.argmin(clusterCenter)):
         euDistOne=np.linalg.norm(sampData[:,i]-sampData[:,clusterCenter[0]],2)
         euDistTwo=np.linalg.norm(sampData[:,i]-sampData[:,clusterCenter[1]],2)
-        if((euDistOne>kDiff)and(euDistOne>euDistTwo)):
+        if(((euDistOne>kDiff)and(euDistOne>euDistTwo))and(euDistTwo<kDiff)):
             clusterTwo=np.append(clusterTwo,i)
-        if((euDistTwo>kDiff)and(euDistOne<euDistTwo)):
+        if(((euDistTwo>kDiff)and(euDistOne<euDistTwo))and(euDistOne<kDiff)):
             clusterOne=np.append(clusterOne,i)
     for i in range(np.argmin(clusterCenter)+1,np.argmax(clusterCenter)):
         euDistOne=np.linalg.norm(sampData[:,i]-sampData[:,clusterCenter[0]],2)
         euDistTwo=np.linalg.norm(sampData[:,i]-sampData[:,clusterCenter[1]],2)
-        if((euDistOne>kDiff)and(euDistOne>euDistTwo)):
+        if(((euDistOne>kDiff)and(euDistOne>euDistTwo))and(euDistTwo<kDiff)):
             clusterTwo=np.append(clusterTwo,i)
-        if((euDistTwo>kDiff)and(euDistOne<euDistTwo)):
+        if(((euDistTwo>kDiff)and(euDistOne<euDistTwo))and(euDistOne<kDiff)):
             clusterOne=np.append(clusterOne,i)
     for i in range(np.argmax(clusterCenter)+1,sampData.shape[1]):
         euDistOne=np.linalg.norm(sampData[:,i]-sampData[:,clusterCenter[0]],2)
         euDistTwo=np.linalg.norm(sampData[:,i]-sampData[:,clusterCenter[1]],2)
-        if((euDistOne>kDiff)and(euDistOne>euDistTwo)):
+        if(((euDistOne>kDiff)and(euDistOne>euDistTwo))and(euDistTwo<kDiff)):
             clusterTwo=np.append(clusterTwo,i)
-        if((euDistTwo>kDiff)and(euDistOne<euDistTwo)):
+        if(((euDistTwo>kDiff)and(euDistOne<euDistTwo))and(euDistOne<kDiff)):
             clusterOne=np.append(clusterOne,i)
+    return clusterCenter,clusterOne,clusterTwo,kDiff
+def logModel():
+    clusterCenter,clusterOne,clusterTwo,kDiff=safeLabels()
     threshOne=0.1
     threshTwo=0.9
-    #^Setting thresholds for classifying
+    #^Setting thresholds for determining whether or not to include new samples into the training set
     while(True):
         print(sampData.shape[1]-clusterOne.shape[0]-clusterTwo.shape[0])
         clusterOneSize=clusterOne.shape[0]
@@ -129,11 +132,11 @@ def fullModel():
         #^acquiring Beta, could/should mess with mu and eta to increase accuracy
         scoreLab=np.array(np.zeros(sampData.shape[1]-clusterOne.shape[0]-clusterTwo.shape[0]))
         scoreLab.fill(-1)
-        #print(scoreLab)
         l=0
         for i in range(0,sampData.shape[1]):
             if(not(((np.any(clusterOne==i))or(np.any(clusterTwo==i))))):
                 scoreLab[l]=recFunctONE(i,iBeta,sampData)
+                #print(scoreLab[l])
                 if(scoreLab[l]>threshTwo):
                     scoreLab[l]=-1
                     clusterTwo=np.append(clusterTwo,i)
@@ -155,7 +158,87 @@ def fullModel():
         if((clusterOneSize==clusterOne.shape[0])and(clusterTwoSize==clusterTwo.shape[0])):
             threshOne=threshOne-0.2*(threshOne-np.average(scoreLab[np.any((scoreLab<0.5)and(scoreLab>-0.1))]))
             threshTwo=threshTwo-0.2*(threshTwo-np.average(scoreLab[np.any(scoreLab>0.5)]))
-            #change 0.5 to higher value to perhaps increase accuracy
+def logModelSlow():
+    #this one may be a bit useless, idk depends on the sample set
+    clusterCenter,clusterOne,clusterTwo,kDiff=safeLabels()
+    while(True):
+        print(sampData.shape[1]-clusterOne.shape[0]-clusterTwo.shape[0])
+        clusterOneSize=clusterOne.shape[0]
+        clusterTwoSize=clusterTwo.shape[0]
+        trainSetX=np.append(sampData[:,clusterOne],sampData[:,clusterTwo],axis=1)
+        trainSetY=np.array(np.zeros(clusterTwo.shape[0]))
+        trainSetY.fill(1)
+        trainSetY=np.append(np.array(np.zeros(clusterOne.shape[0])),trainSetY)
+        trainSetY=trainSetY.reshape((1,-1))
+        iBeta=np.array(np.zeros(sampData.shape[0]))
+        iBeta=iBeta.reshape((-1,1))
+        iBeta=gradientAscent(1e-4,1e-6,iBeta,trainSetX,trainSetY)
+        #^acquiring Beta, could/should mess with mu and eta to increase accuracy
+        scoreLab=np.array(np.zeros(sampData.shape[1]))
+        #scoreLab.fill(None)
+        for i in range(0,sampData.shape[1]):
+            if(not(((np.any(clusterOne==i))or(np.any(clusterTwo==i))))):
+                scoreLab[i]=np.matmul(np.transpose(-iBeta),sampData[:,i])
+        if(scoreLab[np.argmax(scoreLab)]>0):
+            clusterOne=np.append(clusterOne,np.argmax(scoreLab))
+        if(scoreLab[np.argmin(scoreLab)]<0):
+            clusterTwo=np.append(clusterTwo,np.argmin(scoreLab))
+        #^classifying
+        if(clusterOne.shape[0]+clusterTwo.shape[0]==sampData.shape[1]):
+            #create and return labels vector here that matches original positions
+            labels=np.array(np.zeros(sampData.shape[1]))
+            for i in range(0,clusterOne.shape[0]):
+                labels[clusterOne[i]]=0
+            for i in range(0,clusterTwo.shape[0]):
+                labels[clusterTwo[i]]=1
+            return labels
+def eucliDistClass():
+    clusterCenter,clusterOne,clusterTwo,kDiff=safeLabels()
+    setSize=sampData.shape[1]-clusterOne.shape[0]-clusterTwo.shape[0]
+    euclidOne=np.array(np.zeros((clusterOne.shape[0],sampData.shape[1]-clusterOne.shape[0]-clusterTwo.shape[0])))
+    euclidTwo=np.array(np.zeros((clusterTwo.shape[0],sampData.shape[1]-clusterOne.shape[0]-clusterTwo.shape[0])))
+    for i in range(0,setSize):
+        for j in range(0,clusterOne.shape[0]):
+            euclidOne[j,i]=np.linalg.norm(sampData[:,i]-sampData[:,clusterOne[j]],2)
+        for j in range(0,clusterTwo.shape[0]):
+            euclidTwo[j,i]=np.linalg.norm(sampData[:,i]-sampData[:,clusterTwo[j]],2)
+    while(True):
+        setSize=sampData.shape[1]-clusterOne.shape[0]-clusterTwo.shape[0]
+        if(clusterOne.shape[0]+clusterTwo.shape[0]==sampData.shape[1]):
+            #create and return labels vector here that matches original positions
+            labels=np.array(np.zeros(sampData.shape[1]))
+            for i in range(0,clusterOne.shape[0]):
+                labels[clusterOne[i]]=0
+            for i in range(0,clusterTwo.shape[0]):
+                labels[clusterTwo[i]]=1
+            return labels
+        if(clusterOne.shape[0]-clusterTwo.shape[0]>0):
+            kMin=mt.floor(clusterTwo.shape[0]/2)
+        else:
+            kMin=mt.floor(clusterOne.shape[0]/2)
+        normMinOne=np.array(np.zeros(setSize))
+        normMinTwo=np.array(np.zeros(setSize))
+        for i in range(0,setSize):
+            normMinOne[i]=np.sum(euclidOne[range(0,kMin),i])
+            normMinTwo[i]=np.sum(euclidTwo[range(0,kMin),i])
+        winOne=np.array(sampData.shape[0])
+        winTwo=np.array(sampData.shape[0])
+        for i in range(0,setSize):
+            #store indices of 'i' into two arrays indicating which set a sample favors, this makes the next bit easier
+            if(normMinOne[i]>normMinTwo[i]):
+                winOne=np.append(winOne,i)
+            if(normMinOne[i]<normMinTwo[i]):
+                winTwo=np.append(winTwo,i)
+        #something is horridly wrong here, need to retrieve original indices for adding to clusters
+        if(winOne.shape[0]>1):
+            clusterOne=np.append(clusterOne,np.argmin(normMinOne[winOne[1:]]))
+            euclidOne=np.delete(euclidOne,euclidOne[:,np.argmin(normMinOne[winOne[1:]])])
+            euclidTwo=np.delete(euclidTwo,euclidTwo[:,np.argmin(normMinOne[winOne[1:]])])
+        if(winTwo.shape[0]>1):
+            clusterTwo=np.append(clusterTwo,np.argmin(normMinTwo[winTwo[1:]]))
+            euclidOne=np.delete(euclidOne,euclidOne[:,np.argmin(normMinTwo[winTwo[1:]])])
+            euclidTwo=np.delete(euclidTwo,euclidTwo[:,np.argmin(normMinTwo[winTwo[1:]])])
+        
 def error(labels,truthFile):
     truthData=pd.read_csv(truthFile)
     truthData=truthData.T
@@ -177,11 +260,8 @@ def error(labels,truthFile):
             iterate=iterate+1
     errorTwo=(iterate/truthData.shape[1])*100
     if(errorONE>errorTwo):
-         return errorTwo
+        return errorTwo, iterate
     else:
-        return errorOne
+        return errorOne, iterate
 loadData("K_Means_Data.csv")
-print(error(fullModel(),"K_Means_Truth.csv"))
-#Accuracy ideas:
-#line 157
-#increase the power of the logistic regression (more conservative training step)
+print(error(eucliDistClass(),"K_Means_Truth.csv"))
